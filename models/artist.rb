9e147b495955723cdd477765ce6985a8eba35ba8
daniel kelly <danieljkelly91@gmail.com>
2018-02-07 17:16:01 +0000

    added self.find method to artist.rb

--- models/artist.rb
+++ models/artist.rb
@@ -49,4 +49,15 @@ class Artist
     SqlRunner.run(sql, values)
   end
 
+  def self.find(id)
+    sql = "SELECT * FROM artists WHERE id = $1"
+    values = [id]
+    results = SqlRunner.run(sql, values)
+    artist_hash = results.first
+    artist = Artist.new(artist_hash)
+    return artist
+  end
+
+
+
 end
